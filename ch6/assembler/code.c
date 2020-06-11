81bb07ecdbc06ffbd5c6a748dfe56360114808de
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-06-11 21:20:54 +0900

    complete code.c

--- ch6/assembler/code.c
+++ ch6/assembler/code.c
@@ -18,15 +18,67 @@ char *convertDest(char *mnemonic) {
 }
 
 char *convertComp(char *mnemonic) {
-  if(strchr(mnemonic, '0'))
+  if(!strcmp(mnemonic, "0"))
 	return "0101010";
-  if(strchr(mnemonic, '0'))
+  if(!strcmp(mnemonic, "1"))
 	return "0111111";
-  
+  if(!strcmp(mnemonic, "-1"))
+	return "0111010";
+  if(!strcmp(mnemonic, "D"))
+	return "0001100";
+  if(!strcmp(mnemonic, "A"))
+	return "0110000";
+  if(!strcmp(mnemonic, "M"))
+	return "1110000";
+  if(!strcmp(mnemonic, "!D"))
+	return "0001101";
+  if(!strcmp(mnemonic, "!A"))
+	return "0110001";
+  if(!strcmp(mnemonic, "!M"))
+	return "1110001";
+  if(!strcmp(mnemonic, "-D"))
+	return "0001111";
+  if(!strcmp(mnemonic, "-A"))
+	return "0110011";
+  if(!strcmp(mnemonic, "-M"))
+	return "1110011";
+  if(!strcmp(mnemonic, "D+1"))
+	return "0011111";
+  if(!strcmp(mnemonic, "A+1"))
+	return "0110111";
+  if(!strcmp(mnemonic, "M+1"))
+	return "1110111";
+  if(!strcmp(mnemonic, "D-1"))
+	return "0001110";
+  if(!strcmp(mnemonic, "A-1"))
+	return "0110010";
+  if(!strcmp(mnemonic, "M-1"))
+	return "1110010";
+  if(!strcmp(mnemonic, "D+A"))
+	return "0000010";
+  if(!strcmp(mnemonic, "D+M"))
+	return "1000010";
+  if(!strcmp(mnemonic, "D-A"))
+	return "0010011";
+  if(!strcmp(mnemonic, "D-M"))
+	return "1010011";
+  if(!strcmp(mnemonic, "A-D"))
+	return "0000111";
+  if(!strcmp(mnemonic, "M-D"))
+	return "1000111";
+  if(!strcmp(mnemonic, "D&A"))
+	return "0000000";
+  if(!strcmp(mnemonic, "D&M"))
+	return "1000000";
+  if(!strcmp(mnemonic, "D|A"))
+	return "0010101";
+  if(!strcmp(mnemonic, "D|M"))
+	return "1010101";
+  return "101010";
 }
 
 char *convertJump(char *mnemonic) {
-  if(mnemonic == "")
+  if(mnemonic == NULL)
 	return "000";
   if(!strcmp(mnemonic, "JGT"))
 	return "001";
@@ -42,5 +94,5 @@ char *convertJump(char *mnemonic) {
 	return "110";
   if(!strcmp(mnemonic, "JMP"))
 	return "111";
-  return "";
+  return "000";
 }
